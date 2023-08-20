c6142e49e5f361b81f09495e6e6fe6fadc7dc514
freekos <gazizbolat2@gmail.com>
2023-08-20 12:59:19 +0600

    feat: add root-page, page-styles, layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,141 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./page.module.scss";
+import { Template } from "@/shared/ui";
+
+interface Task {
+	value: string;
+}
+interface Interval {
+	startTime: string;
+	endTime: string;
+	tasks: Task[];
+}
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [intervals, setIntervals] = useState<Interval[]>([]);
+	const [currentInterval, setCurrentInterval] = useState<Interval | null>(null);
+	const [intervalValue, setIntervalValue] = useState<number>(1);
+	const [taskValue, setTaskValue] = useState<string>("");
+	const [timer, setTimer] = useState<number>(0);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	function handleStartInterval() {
+		const startTime = new Date(Date.now());
+		console.log(startTime.getTime());
+		const endTime = new Date(startTime);
+		endTime.setHours(endTime.getHours() + intervalValue);
+		setCurrentInterval({
+			startTime: startTime.toISOString(),
+			endTime: endTime.toISOString(),
+			tasks: [],
+		});
+		setTimer(intervalValue * 60 * 60);
+	}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+	function handleChangeInterval(event: any) {
+		const target = event.target;
+		const value = target.value;
+		const isNumber = !isNaN(Number(value));
+		if (!isNumber) return;
+		const interval = Number(value);
+		if (interval < 0) {
+			setIntervalValue(0);
+			return;
+		} else if (interval > 12) {
+			setIntervalValue(12);
+			return;
+		}
+		setIntervalValue(interval);
+	}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+	function handleAddTask() {
+		setCurrentInterval((prev) => {
+			if (prev) {
+				return { ...prev, tasks: [{ value: taskValue }, ...prev.tasks] };
+			}
+			return prev;
+		});
+	}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+	useEffect(() => {
+		if (timer === 0) return;
+		const timeout = setTimeout(() => {
+			setTimer((prev) => prev - 1);
+		}, 1000);
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [timer]);
+
+	return (
+		<Template.Main>
+			<Template.Section className={styles.hero}>
+				<div className={styles.hero__timer_wrap}>
+					<div className={styles.hero__timer}>{getHhMmSs(new Date(), timer)}</div>
+				</div>
+
+				<div className={styles.hero__interval}>
+					<div className={styles.textfield}>
+						<input
+							className={styles.textfield__input}
+							value={intervalValue > 9 ? intervalValue : "0" + intervalValue}
+							onChange={handleChangeInterval}
+							onBlur={() => setIntervalValue(1)}
+						/>
+						<span className={styles.textfield__icon}>Hours</span>
+					</div>
+					<button
+						className={styles.hero__start}
+						disabled={!!currentInterval}
+						onClick={handleStartInterval}>
+						Start
+					</button>
+				</div>
+
+				<div className={styles.task}>
+					<div className={styles.task__time}>
+						{currentInterval && <span>{getHhMm(new Date(currentInterval.startTime))}</span>}
+						<div className={styles.br} style={{ width: "10vw" }} />
+						<span>Now</span>
+						<div className={styles.br} style={{ width: "10vw" }} />
+						{currentInterval && <span>{getHhMm(new Date(currentInterval.endTime))}</span>}
+					</div>
+					<h1 className={styles.task__title}>Create Task</h1>
+					<div className={styles.task__textfield}>
+						<input
+							className={styles.task__input}
+							value={taskValue}
+							onChange={(event) => setTaskValue(event.target.value)}
+						/>
+						<button className={styles.task__add} onClick={handleAddTask}>
+							Add
+						</button>
+					</div>
+
+					<div className={styles.task__list}>
+						{currentInterval?.tasks.map((task, index) => (
+							<div key={index} className={styles.task__item}>
+								{task.value}
+							</div>
+						))}
+					</div>
+				</div>
+			</Template.Section>
+		</Template.Main>
+	);
+}
+
+function getHhMm(date: Date) {
+	const hours = date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
+	const minutes = date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+	return `${hours}:${minutes}`;
+}
+
+function getHhMmSs(date: Date, setSeconds: number) {
+	date.setHours(0, 0, setSeconds, 0);
+	const hours = date.getHours() > 9 ? date.getHours() : "0" + date.getHours();
+	const minutes = date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+	const seconds = date.getSeconds() > 9 ? date.getSeconds() : "0" + date.getSeconds();
+	return `${hours}:${minutes}:${seconds}`;
 }
