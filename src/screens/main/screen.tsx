5d3855f49f13f10540dbec03cc7ce4ac882d2852
freekos <gazizbolat2@gmail.com>
2023-08-27 14:00:11 +0600

    refactor(screens/main): change title-head place

--- src/screens/main/screen.tsx
+++ src/screens/main/screen.tsx
@@ -87,15 +87,14 @@ export function MainScreen() {
 		<Template.Main>
 			<Template.Section className={styles.hero}>
 				<Template.Container>
-					<div className={styles.hero__head}>
-						<h1 className={styles.hero__title}>Time Action</h1>
-						<p className={styles.hero__description}>
-							Is a time interval monitoring application that can integrate with different task
-							managers(Notion, Trello, etc.) and analyze your actions
-						</p>
-					</div>
-
 					<div className={styles.hero__wrapper}>
+						<div className={styles.hero__head}>
+							<h1 className={styles.hero__title}>Time Action</h1>
+							<p className={styles.hero__description}>
+								Is a time interval monitoring application that can integrate with different task
+								managers(Notion, Trello, etc.) and analyze your actions
+							</p>
+						</div>
 						<header className={styles.hero__header}>
 							{currentInterval ? (
 								<TimeView
